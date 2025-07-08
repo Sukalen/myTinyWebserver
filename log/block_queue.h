91b249607d516ad09d73ebd3b4085d5aea2549e9
Suu <2841512166@qq.com>
2025-07-08 22:55:04 +0800

    add locker

---
+++ log/block_queue.h
@@ -0,0 +1,14 @@
+#ifndef BLOCK_QUEUE_H
+#define BLOCK_QUEUE_H
+
+#include<iostream>
+#include<stdlib.h>
+#include<pthread.h>
+#include<sys/time.h>
+#include "../lock/locker.h"
+
+
+
+
+
+#endif
---
+++ log/log.h
@@ -0,0 +1,34 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<stdarg.h>
+#include<pthread.h>
+#include "block_queue.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
